9f804ac57740e50e610a3a50c68aa6e31a112ac8
Adossi Fred William <f.adossi@alustudent.com>
2026-07-05 16:07:52 +0200

    Add MongoDB design, loader and queries

--- database/mongodb/queries.js
+++ database/mongodb/queries.js
@@ -16,11 +16,6 @@ db.readings.find(
   { _id: 0, datetime: 1, total_consumption: 1 }
 ).sort({ datetime: 1 }).limit(3)
 
-// Expected result is the first three of the day:
-// { datetime: 2017-01-01T00:00:00Z, total_consumption: 70425.53 }
-// { datetime: 2017-01-01T00:10:00Z, total_consumption: 69320.84 }
-// { datetime: 2017-01-01T00:20:00Z, total_consumption: 67803.22 }
-
 
 // Query 2 finds the average total demand for each hour of day.
 // It uses a group stage and confirms the evening peak.
@@ -35,11 +30,6 @@ db.readings.aggregate([
   { $limit: 3 }
 ])
 
-// Expected result top three hours:
-// { _id: 20, avg_total: about 98037 }
-// { _id: 19, avg_total: about 95645 }
-// { _id: 21, avg_total: about 94632 }
-
 
 // Query 3 finds the newest reading.
 // It sorts by time from new to old and takes one document.
@@ -48,8 +38,6 @@ db.readings.find(
   { _id: 0, datetime: 1, total_consumption: 1, zones: 1 }
 ).sort({ datetime: -1 }).limit(1)
 
-// Expected result is the last timestamp in the data:
-// { datetime: 2017-12-30T23:50:00Z, zones: [ ... ], total_consumption: about 65750 }
 
 
 // Query 4 is a bonus that finds the average consumption per zone.
@@ -60,7 +48,3 @@ db.readings.aggregate([
   { $sort: { _id: 1 } }
 ])
 
-// Expected result is about:
-// { _id: 1, avg_consumption: 32344.97 }
-// { _id: 2, avg_consumption: 21042.51 }
-// { _id: 3, avg_consumption: 17835.41 }
